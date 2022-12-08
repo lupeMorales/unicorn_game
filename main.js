06310ced34c2e5daa5a5587be3816969da5011d8
modehans <monicaglezsanch@gmail.com>
2022-12-08 12:11:05 +0100

    Import Lollipop one in main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-
-"use strict";
-import "./components/lm-lollipoptwo.js";
-import "./components/lm-unicorn.js";
-import "./components/lm-cloud";
+'use strict';
+import './components/lm-lollipoptwo.js';
+import './components/lm-lollipop.js';
+import './components/lm-unicorn.js';
+import './components/lm-cloud';
